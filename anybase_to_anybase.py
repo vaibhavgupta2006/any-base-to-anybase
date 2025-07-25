41e43229e6e32470ceb1d385ce065c8e9e0ee98f
vaibhavgupta2006 <vg3229565@gmail.com>
2025-07-25 20:31:50 +0530

    Update anybase_to_anybase.py

--- anybase_to_anybase.py
+++ anybase_to_anybase.py
@@ -2,25 +2,24 @@ data="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 def anybase_to_decimal(data):
     number=input("Enter the number: ").upper()
     base=int(input("enter the base: "))
-    f=0
-    dec=0
+    power=0
+    decimal_number=0
     for digit in number[::-1]:
         if data.index(digit)>base:
             return print("you have entered wrong number or base")
-        dec=dec+data.index(digit)*(base**f)
-        f+=1
-    return dec
+        decimal_number= decimal_number+data.index(digit)*(base**power)
+        power+=1
+    return decimal_number
 
-decimal=(anybase_to_decimal(data))
-end_base=int(input("enter the base you want to convert this number to: "))
-def decimal_to_anybase(data,decimal,end_base):
+decimal_number=(anybase_to_decimal(data))
+target_base=int(input("enter the base you want to convert this number to: "))
+def decimal_to_anybase(data,decimal_number,target_base):
     desired_number=""
-    while decimal>0:
-        rem=decimal%end_base
-        desired_number=desired_number+data[rem]
-        decimal=decimal//end_base
+    while decimal_number>0:
+        remainder=decimal_number%target_base
+        desired_number=desired_number+data[remainder]
+        decimal_number=decimal_number//target_base
       
     return desired_number[::-1]
     
-print(f"your desired number in base {end_base} is : {decimal_to_anybase(data,decimal,end_base)}")
-
+print(f"your desired number in base {target_base} is : {decimal_to_anybase(data,decimal_number,target_base)}")
